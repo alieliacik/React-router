7b9a5401f3dc16c2deec0ceec433281b82f38264
Alex Eliacik <57728302+alexeliacik@users.noreply.github.com>
2020-01-07 06:36:20 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HashRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
